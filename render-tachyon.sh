813f9c8e1d2480e0a6fe2386c5e02623bf3ac2ee
Patrick Melix <patrick.melix@uni-leipzig.de>
2023-05-17 14:15:45 +0200

    Added render-tachyon and traj2movie scripts.

---
+++ render-tachyon.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# render all .dat files given as arguments with tachyon renderer
+# after rendering, convert to png and trim
+# usage: render-tachyon.sh *.dat
+set -e
+set -o pipefail
+shopt -s globstar
+
+args=("$@")
+
+n=0
+
+for ((n=0; n<$#; ++n));
+do
+   file=${args[$n]}
+   if [ ! -f "$file" ]; then
+      continue
+   fi
+
+   echo "$file"
+   basefile=$(basename $file .dat)
+   path=$(dirname $file)
+   sed -i 's/Resolution.*/Resolution 7664 4164/' "${file}"
+   tachyon  -aasamples 12 ${file} -format TARGA -o "${path}"/"${basefile}".tga
+   #tga2png.sh ${basefile}.tga
+   convert -trim "${path}"/"${basefile}".tga "${path}"/"${basefile}".png
+   rm "${path}"/"${basefile}".tga
+done
